aa91801bbe4b158d0784933f7680ba29125f9a15
tu <t.u.9293991@gmail.com>
2026-02-11 18:38:46 +0900

    front

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,6 +1,7 @@
 "use client"
 
 import { useState, useEffect } from "react";
+import t from "./page.module.css";
 
 export default function Home() {
   const [msg, setMsg] = useState("loding..");
@@ -10,40 +11,41 @@ export default function Home() {
       .then(setMsg)
       .catch(() => setMsg("fetch Error"));
   },[]);
+
   return (
-    <main>
+    <main className={t.main}>
       <h1>Cafeteria Page</h1>
-      <section>
+      <section className={t.infoContainer}>
         <h2>会員登録・ログイン</h2>
-        <nav>
-          <a href="/user/signin">Signup</a>
+        <nav className={t.infoContents}>
+          <label>登録・ログインは<a href="/user/signin">ここから</a>飛ぶ🚀</label>
         </nav>
       </section>
-      <section>
+      <section className={t.infoContainer}>
         <h2>Menu List</h2>
-        <nav>
+        <nav className={t.infoContents}>
           <p></p>
-          <a href="/menu">MenuList</a>
+          <label>メニュー一覧は<a href="/menu">ここから</a>飛ぶ🚀</label>
         </nav>
       </section>
-      <section>
+      <section className={t.infoContainer}>
         <h2>Order</h2>
-        <nav>
-          <a href="/order">Order</a>
+        <nav className={t.infoContents}>
+          <label>注文は<a href="/order">ここから</a>飛ぶ🚀</label>
         </nav>
       </section>
-      <section>
+      <section className={t.infoContainer}>
         <h2>Shop Info</h2>
-        <nav>
-          <p>Address : Tokyo </p>
-          <p>Tel : 000-xxxx-1234</p>
-          <p>E-Mail : cafeteria@sample.co.jp</p>
+        <nav className={t.infoContents}>
+          <p><span>Address</span> : Tokyo </p>
+          <p><span>Tel</span> : 000-xxxx-1234</p>
+          <p><span>E-Mail</span> : cafeteria@sample.co.jp</p>
         </nav>
       </section>
-      <section>
+      <section className={t.infoContainer}>
         <h2>Recruit</h2>
-        <nav>
-          <a href="/recruit">Recruit</a>
+        <nav className={t.infoContents}>
+          <label>求人情報は<a href="/recruit">ここから</a>飛ぶ🚀</label>
         </nav>
       </section>
     </main>
